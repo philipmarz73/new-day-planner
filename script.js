92af717d9a40dafd1553c4dad8c5cbe3979890ec
Phlipper <marzullop73@gmail.com>
2021-02-12 17:49:37 -0500

    added current day display w/JQuery and moment.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
     console.log("hello world");
+
+// set jQuery document ready function enabling page manipulation
+// checked link to HTML with console.log
+
+// set up Moment.js to show current time and date
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 });
