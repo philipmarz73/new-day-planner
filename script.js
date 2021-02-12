4e882583e186f3e55c147a558699a66d2e353542
Phlipper <marzullop73@gmail.com>
2021-02-12 17:56:32 -0500

    setup for time block colors based on past, present, future

--- script.js
+++ script.js
@@ -6,4 +6,29 @@ $(document).ready(function(){
 
 // set up Moment.js to show current time and date
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+function setTimeBlockColor() {
+    if(parseInt(hour) < 09){
+       $("#nineText").addClass("future");
+       $("#tenText").addClass("future");
+       $("#elevenText").addClass("future");
+       $("#noonText").addClass("future");
+       $("#oneText").addClass("future");
+       $("#twoText").addClass("future");
+       $("#threeText").addClass("future");
+       $("#fourText").addClass("future");
+       $("#fiveText").addClass("future");
+       $("#sixText").addClass("future");
+    }
+
+
+
+
+
+
+
+
+
+
+
 });
